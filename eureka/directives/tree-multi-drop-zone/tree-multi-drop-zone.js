99e6326d8c97c4ffcecf7865ea39ec81770ec620
JSONS <jsands79@gmail.com>
2017-07-21 00:02:39 -0400

    more changes to drop zone validation.  Making progress can submit form from directive if drop zone is not valid and is later corrected

--- eureka/directives/tree-multi-drop-zone/tree-multi-drop-zone.js
+++ eureka/directives/tree-multi-drop-zone/tree-multi-drop-zone.js
@@ -7,42 +7,27 @@
 
     function TreeMultiDropZone() {
         return {
-            require: 'ngModel',
+            require: ['^form', 'ngModel'],
             restrict: 'AE',
-            link: function(scope, element, attr, ngModel) {
-                let myForm = $("#categorizationForm");
+            link: function(scope, element, attr, ctrls) {
+                let myForm = $("#categorizationForm"); // easy way to get form to user listener
+                scope.form = ctrls[0]; // require both form and ngmodel, may only need ngmodel.  Will remove if I do not need both.
+                let ngModel = ctrls[1]; // read above
 
-                scope.on("submit", function(event) {
-                    event.preventDefault();
-
+                myForm.on("submit", function(event) {
                     if (scope.vm.items.length > 0) {
                         console.log('drop zone is valid');
-                        $('#tree-container').removeClass('trigger-validation');
-                        $('#error-label').addClass('hide-item');
-
-                        scope.$apply(function() {
-                            scope.$parent.categorizationForm.$valid = true;
-                            scope.$parent.categorizationForm.$submitted = true;
-                            scope.$parent.categorizationForm.definition.$setValidity('definition', true);
-                            scope.$parent.categorizationForm.definition.$setDirty(false);
-                            scope.$eval(element.context.parentElement.parentNode.attributes["angular-validator-submit"].value);
-                        });
-
+                        scope.$parent.categorizationForm.definition.$setValidity('definition', true);
+                        $('#tree-container').removeClass('trigger-validation'); 
+                        $('#error-label').addClass('hide-item'); // remove validation classes, will use bootstrap eventually
+                        scope.$parent.editPhenotype.save();  // calling the save function of the controller.  Will need to make this dynamic and not hard coded
                     } else if (scope.vm.items.length === 0 && ngModel.$$parentForm.submitted) {
-
-                        scope.$apply(function() {
-                            scope.$parent.categorizationForm.$valid = false;
-                            scope.$parent.categorizationForm.$submitted = false;
-                            scope.$parent.categorizationForm.definition.$setValidity('definition', false);
-                            scope.$parent.categorizationForm.definition.$setDirty(true);
-                        });
+                         event.preventDefault();
                         $('#tree-container').addClass('trigger-validation');
-                        $('#error-label').removeClass('hide-item');
-
+                        $('#error-label').removeClass('hide-item');  // remove validation classes, will use bootstrap eventually
                         console.log('Form has been submitted, but is not valid');
                     }
                 })
-
             },
             scope: {
                 items: '=',
