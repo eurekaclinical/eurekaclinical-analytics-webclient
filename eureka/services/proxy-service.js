10393c582da69cc09b08dcb692df56a37b89d60e
Andrew Post <arpost@emory.edu>
2017-11-04 11:31:01 -0400

    Assume we’re using CORS.

--- eureka/services/proxy-service.js
+++ eureka/services/proxy-service.js
@@ -45,7 +45,7 @@
 	function getSession() {
 	    return ConfigFileService.getConfig()
 		.then(function (data) {
-		    return getEurekaClinicalSession(data.eurekaWebappUrl)
+		    return getEurekaClinicalSession()
 			.then(handleSuccess, handleError);
 		}, handleError);
 	}
@@ -81,44 +81,9 @@
 	    return ($q.reject(response.data));
         }
 
-	function getEurekaClinicalSession(eurekaWebappUrl) {
-	    
-	    return $q((resolve, reject) => {
-		let theIframe = document.createElement('iframe');
-		theIframe.src = eurekaWebappUrl + '/protected/get-session';
-		let timeout = null;
-		
-		function receiveMessage(event) {
-		    theIframe = document.body.removeChild(theIframe);
-		    window.removeEventListener('message', receiveMessage, false);
-		    if (timeout) {
-			clearTimeout(timeout);
-		    }
-		    var origin = event.origin || event.originalEvent.origin;
-		    if (eurekaWebappUrl.startsWith(origin)) {
-			resolve({data: ''});
-		    } else {
-			reject({data: {status: 400, statusText: 'Bad Request'}});
-		    }
-		}
-		window.addEventListener('message', receiveMessage, false);
-
-		function onTimeout() {
-		    window.removeEventListener('message', receiveMessage);
-		    theIframe = document.body.removeChild(theIframe);
-		    reject({status: 400, statusText: 'Bad Request'});
-		}
-
-		theIframe.style.display = 'none';
-		theIframe = document.body.appendChild(theIframe);
-		if (CookieService.get()) {
-		    timeout = setTimeout(onTimeout, 1000 * 30);
-		} else {
-		    window.removeEventListener('message', receiveMessage);
-		    reject({data: {status: 400, statusText: 'Bad Request'}});
-		}
-	    });
-	    
+	function getEurekaClinicalSession() {
+	    return $http.get(dataProtectedEndPoint + '/get-session')
+		.then(handleSuccess, handleError);
 	}
 
     }
